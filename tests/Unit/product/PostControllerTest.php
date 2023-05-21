b240b47e5dbdf583a68e97c126c60542ecf758c4
bonheurtuyi <bonheurtuyikunde2@outlook.com>
2023-05-21 12:49:07 +0200

    =finishing inventory

---
+++ tests/Unit/product/PostControllerTest.php
@@ -0,0 +1,17 @@
+<?php
+
+
+use Tests\TestCase;
+
+class PostControllerTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     */
+    public function test_example(): void
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+}
